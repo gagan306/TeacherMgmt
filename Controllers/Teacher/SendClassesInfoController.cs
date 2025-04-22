ce4b8575d83fbeb24375467fcf7ccd15a1a4233d
Gagan Bhusal <bhusalgagan612@gmail.com>
2025-04-22 14:03:49 +0545

    planning phase

---
+++ Controllers/Teacher/SendClassesInfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeacherMgmt.Controllers.Teacher
+{
+    public class SendClassesInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
